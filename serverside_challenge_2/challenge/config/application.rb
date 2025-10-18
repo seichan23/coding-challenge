d0dda1f45e76a8488b16aa3f4bf2cb6755b6f431
seichan23 <chan@channoMacBook-Pro.local>
2025-10-18 10:00:21 +0900

    time_zoneの設定を追加

--- serverside_challenge_2/challenge/config/application.rb
+++ serverside_challenge_2/challenge/config/application.rb
@@ -10,6 +10,7 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = ENV.fetch('TZ', 'Asia/Tokyo')
 
     # Configuration for the application, engines, and railties goes here.
     #
