a89c86314b1d9777ba87e6cc7dbd8151247b48f0
seichan23 <chan@channoMacBook-Pro.local>
2025-10-20 01:32:13 +0900

    Planモデルに関連付けを追加してモデルのテストを追加

--- serverside_challenge_2/challenge/app/models/plan.rb
+++ serverside_challenge_2/challenge/app/models/plan.rb
@@ -23,6 +23,7 @@
 class Plan < ApplicationRecord
   include CsvImportable
   belongs_to :provider
+  has_many :basic_charges, dependent: :destroy
 
   validates :name, presence: true
   validates :code, presence: true, uniqueness: { scope: :provider_id }
