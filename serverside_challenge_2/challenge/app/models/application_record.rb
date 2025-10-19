7e42dbd58808aaddb8176841d5ab2e80d0b7a2ab
seichan23 <chan@channoMacBook-Pro.local>
2025-10-20 01:54:30 +0900

    railsの実装を参考に最大数値を定数化

--- serverside_challenge_2/challenge/app/models/application_record.rb
+++ serverside_challenge_2/challenge/app/models/application_record.rb
@@ -2,4 +2,7 @@
 
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  # https://github.com/rails/rails/blob/b180e2c522c372f5d028d6a5113e69ff416128ab/activemodel/lib/active_model/type/integer.rb#L100
+  INTEGER_MAX = ActiveRecord::Type::Integer.new.__send__(:max_value)
 end
