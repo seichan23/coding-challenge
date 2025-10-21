c3e864ec7513718a930be9dfcd65c2ee9129061b
seichan23 <chan@channoMacBook-Pro.local>
2025-10-21 19:28:55 +0900

    seed実行時にUsageChargeのデータもインポートするように

--- serverside_challenge_2/challenge/db/seeds.rb
+++ serverside_challenge_2/challenge/db/seeds.rb
@@ -1,4 +1,5 @@
 Provider.import_from_csv!(Rails.root.join('db/csv/providers.csv'))
 Plan.import_from_csv!(Rails.root.join('db/csv/plans.csv'))
 BasicCharge.import_from_csv!(Rails.root.join('db/csv/basic_charges.csv'))
+UsageCharge.import_from_csv!(Rails.root.join('db/csv/usage_charges.csv'))
 puts 'seed完了'
