811144765501fbadc13447a52b5febe99f558be5
seichan23 <chan@channoMacBook-Pro.local>
2025-10-21 19:20:03 +0900

    UsageChargeモデルにバリデーション追加とモデルのテストを追加

--- serverside_challenge_2/challenge/app/models/usage_charge.rb
+++ serverside_challenge_2/challenge/app/models/usage_charge.rb
@@ -22,5 +22,52 @@
 #  fk_rails_...  (plan_id => plans.id)
 #
 class UsageCharge < ApplicationRecord
+  MAX_AMOUNT = 999_999.99
   belongs_to :plan
+
+  validates :from_kwh, :unit_price, presence: true
+  validates :from_kwh, uniqueness: { scope: :plan_id }
+  validates :from_kwh, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: INTEGER_MAX,
+  }
+  validates :to_kwh, numericality: {
+    only_integer: true,
+    allow_nil: true,
+  }
+  validates :unit_price, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: MAX_AMOUNT,
+  }
+  validate :validate_to_kwh_greater_than_from_kwh
+  validate :validate_range_must_not_overlap
+
+  private
+
+  def validate_to_kwh_greater_than_from_kwh
+    return if to_kwh.blank? || from_kwh.blank?
+    return if to_kwh > from_kwh
+
+    errors.add(:to_kwh, "は#{self.class.human_attribute_name(:from_kwh)}より大きくなければなりません")
+  end
+
+  def validate_range_must_not_overlap
+    return if from_kwh.blank?
+    return if plan.blank?
+
+    errors.add(:base, '使用量範囲が他の区分と重複しています') if overlapping_range_exists?
+  end
+
+  def overlapping_range_exists?
+    plan.usage_charges
+      .where.not(id: id)
+      .where(
+        '(from_kwh <= :to_kwh OR :to_kwh IS NULL)
+        AND (:from_kwh <= to_kwh OR to_kwh IS NULL)',
+        from_kwh: from_kwh,
+        to_kwh: to_kwh,
+      )
+      .exists?
+  end
 end
--- serverside_challenge_2/challenge/test/models/usage_charge_test.rb
+++ serverside_challenge_2/challenge/test/models/usage_charge_test.rb
@@ -24,5 +24,70 @@
 require 'test_helper'
 
 class UsageChargeTest < ActiveSupport::TestCase
-  belong_to(:plan)
+  setup do
+    @plan = create(:plan)
+  end
+
+  subject { build(:usage_charge) }
+  should belong_to(:plan)
+
+  should validate_presence_of(:from_kwh)
+  should validate_uniqueness_of(:from_kwh).scoped_to(:plan_id)
+  should validate_numericality_of(:from_kwh)
+    .only_integer
+    .is_greater_than_or_equal_to(0)
+    .is_less_than_or_equal_to(ApplicationRecord::INTEGER_MAX)
+  should validate_numericality_of(:to_kwh)
+    .only_integer
+    .allow_nil
+  should validate_presence_of(:unit_price)
+  should validate_numericality_of(:unit_price)
+    .is_greater_than_or_equal_to(0)
+    .is_less_than_or_equal_to(UsageCharge::MAX_AMOUNT)
+
+  test 'from_kwhがto_kwhより小さい場合は有効' do
+    usage_charge = build(:usage_charge, plan: @plan, from_kwh: 101, to_kwh: 200)
+
+    assert { usage_charge.valid? }
+  end
+
+  test 'from_kwhがto_kwhより大きい場合は無効' do
+    usage_charge = build(:usage_charge, plan: @plan, from_kwh: 20, to_kwh: 10)
+
+    assert { usage_charge.invalid? }
+    assert { usage_charge.errors.full_messages.include?('使用量(kwh)の上限は使用量(kwh)の下限より大きくなければなりません') }
+  end
+
+  test '使用量範囲が他の使用量範囲と重複していない場合は有効' do
+    create(:usage_charge, plan: @plan, from_kwh: 10, to_kwh: 20)
+    usage_charge = build(:usage_charge, plan: @plan, from_kwh: 21, to_kwh: 30)
+
+    assert { usage_charge.valid? }
+  end
+
+  test '使用量範囲が他の使用量範囲と重複している場合は無効' do
+    create(:usage_charge, plan: @plan, from_kwh: 10, to_kwh: 20)
+    usage_charge = build(:usage_charge, plan: @plan, from_kwh: 20, to_kwh: 30)
+
+    assert { usage_charge.invalid? }
+    assert { usage_charge.errors.full_messages.include?('使用量範囲が他の区分と重複しています') }
+  end
+
+  test 'to_kwhがnilの場合でも重複チェックが正しく動作する' do
+    create(:usage_charge, plan: @plan, from_kwh: 0, to_kwh: nil)
+    usage_charge = build(:usage_charge, plan: @plan, from_kwh: 1, to_kwh: nil)
+
+    assert { usage_charge.invalid? }
+    assert { usage_charge.errors.full_messages.include?('使用量範囲が他の区分と重複しています') }
+  end
+
+  test '別のプランの使用量範囲と重複している場合は有効' do
+    provider = create(:provider)
+    plan1 = create(:plan, provider: provider)
+    plan2 = create(:plan, provider: provider)
+    create(:usage_charge, plan: plan1, from_kwh: 0, to_kwh: nil)
+    usage_charge = build(:usage_charge, plan: plan2, from_kwh: 1, to_kwh: nil)
+
+    assert { usage_charge.valid? }
+  end
 end
