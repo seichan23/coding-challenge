505e3d87e6ad81df46f3c8518ca552ee6b534320
seichan23 <chan@channoMacBook-Pro.local>
2025-10-29 00:25:49 +0900

    apiの本番環境にcorsの設定を追加

--- serverside_challenge_2/challenge/api/config/environments/production.rb
+++ serverside_challenge_2/challenge/api/config/environments/production.rb
@@ -83,4 +83,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins ENV.fetch('ALLOWED_ORIGIN')
+      resource '/api/v1/electricity_charges',
+        headers: :any,
+        methods: [:get]
+    end
+  end
 end
